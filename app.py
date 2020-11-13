009616eb352315f23c37032cf400bed8cf800fa5
unknown <nicolopinci.1997@gmail.com>
2020-11-13 18:55:21 +0100

    Dataset integration

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ import dash_daq as daq
 import dash_draggable
 import json
 import pickle
+import time
 
 import plotly.graph_objs as go
 from dash.exceptions import PreventUpdate
@@ -37,7 +38,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets, external_sc
 
 
 class GraphInfo:
-    def __init__(self, dataset, title, graph_type = "line", axes = [], color = None, filters = [], animation = None, map_type = 'choropleth', location_mode = 'country names', colorscale='Portland', animation_frame = 'data["date"].astype(str)', min_animation = 0, max_animation = 1, plot_type = "lines", hide_side_legend = False):
+    def __init__(self, dataset, title, graph_type = "line", axes = [], color = None, filters = [], animation = None, map_type = 'choropleth', location_mode = 'country names', colorscale='Portland', animation_frame = 'data["date"].astype(str)', min_animation = 0, max_animation = 1, plot_type = "lines", hide_side_legend = False, width = 650, height = 500):
         self.title = title 
         self.axes = axes
         self.color = color
@@ -53,6 +54,8 @@ class GraphInfo:
         self.max_animation = max_animation
         self.plot_type = plot_type
         self.hide_side_legend = hide_side_legend
+        self.width = width
+        self.height = height
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
@@ -65,7 +68,7 @@ class Axis:
         self.labels = labels
 
 class Filter:
-    def __init__(self, column_name = "", default_value = [], multi = True, filter_type="Dropdown", start_date = "2020-01-01", end_date="2020-12-31", show_on_marker = False):
+    def __init__(self, column_name = "", default_value = [], multi = True, filter_type="Dropdown", start_date = "2020-01-01", end_date="2020-12-31", show_on_marker = False, filter_name = ""):
         self.column_name = column_name
         self.default_value = default_value
         self.multi = multi
@@ -73,6 +76,7 @@ class Filter:
         self.start_date = start_date
         self.end_date = end_date
         self.show_on_marker = show_on_marker
+        self.filter_name = filter_name
 
 class Animation:
     def __init__(self, active = False, axis_number = None):
@@ -93,11 +97,12 @@ def static_file(path):
 project_name = "CODAV"
 
 def mask_max(in_data, filter_key):
-    in_data[in_data[filter_key] != in_data[filter_key]] = in_data[filter_key].min()
-    in_data[in_data[filter_key] != in_data[filter_key]] = 0
 
-    out_data = in_data[in_data[filter_key] == in_data[filter_key].max()]
-    return out_data
+    in_data[filter_key].fillna(in_data[filter_key].min())
+    in_data[filter_key].fillna(0)
+
+    return in_data[in_data[filter_key] == in_data[filter_key].max()]
+   
 
 def generate_layout():
     return html.Div(
@@ -109,6 +114,20 @@ def generate_layout():
         children=html.Div([
             html.I("", className="fas fa-upload")
         ]),
+               
+        # Do not allow multiple files to be uploaded
+        multiple=False
+        ),
+
+
+        dcc.Upload(
+        id='upload-school',
+        children=html.Div([
+              html.I("", className="fas fa-smile")
+        ]),
+
+
+
        
         # Do not allow multiple files to be uploaded
         multiple=False
@@ -128,39 +147,50 @@ def generate_layout():
         html.Div(id="filter_equivalence", style={'display': 'none'}),
 
         # Hidden div inside the app that stores the intermediate value
-        html.Div(id='saved_data', style={'display': 'none'})
+        html.Div(id='saved_data', style={'display': 'none'}),
+        html.Div(id='saved_school', style={'display': 'none'})
+
     ],
     )
 
 app.layout = generate_layout
 
 
-def parse_contents(contents):
-    content_type, content_string = contents.split(',')
+def parse_contents(covid, school):
+    covid_content_type, covid_content_string = covid.split(',')
+    school_content_type, school_content_string = school.split(',')
 
-    decoded = base64.b64decode(content_string)
+    covid_decoded = base64.b64decode(covid_content_string)
+    school_decoded = base64.b64decode(school_content_string)
 
-    try:
-        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-      
-        return df.to_json(date_format='iso', orient='split')
+    school = pd.read_csv(io.StringIO(school_decoded.decode('utf-8')))
+    covid = pd.read_csv(io.StringIO(covid_decoded.decode('utf-8')))
 
 
-    except Exception as e:
-        return html.Div([
-            'There was an error processing this file.'
-        ])
+    school['date'] = pd.to_datetime(school['date'])
+    covid['date'] = pd.to_datetime(covid['date'])
+
+    school['date'] = school['date'].dt.strftime("%Y-%m-%d")
+    covid['date'] = covid['date'].dt.strftime("%Y-%m-%d")
+
+    combined_datasets = pd.merge(covid, school, how = 'left', right_on = ['date', 'iso_code'], left_on = ['date', 'iso_code'])
+ 
+    return combined_datasets.to_json(date_format='iso', orient='split')
+
+
 
 
 
 
 @app.callback(Output('saved_data', 'children'),
-              [Input('upload-data', 'contents')])
-def update_output(contents):
-    if contents is not None:
-        return parse_contents(contents)
+              [Input('upload-data', 'contents'), Input('upload-school', 'contents')])
+def update_output(covid, school):
+    if covid is not None and school is not None:
+        return parse_contents(covid, school)
     else:
         raise dash.exceptions.PreventUpdate
+
+
 
 
 
@@ -168,22 +198,30 @@ def update_output(contents):
     
 @app.callback([Output("graphs_container", "children")],
 [Input("saved_data", "children")])
+def initialize_graphs(jsonified_data):
+    if(jsonified_data is not None):
+        return add_preset(jsonified_data)
+    else:
+        raise dash.exceptions.PreventUpdate
+
+
+
 def add_preset(jsonified_data):
 
     graph_divs = []
  
     # Graph 3: cumulative tests, confirmed cases, deaths per million people
     axes = []
-    axes.append(Axis("x", True, 'data["date"]'))
-    axes.append(Axis("y", True, ['data["total_tests_per_thousand"]*1000', 'data["total_cases_per_million"]', 'data["total_deaths_per_million"]'], ["Total tests", "Total cases", "Total deaths"]))
+    axes.append(Axis("Date", True, 'data["date"]'))
+    axes.append(Axis("Tests, cases and deaths per million", True, ['data["total_tests_per_thousand"]*1000', 'data["total_cases_per_million"]', 'data["total_deaths_per_million"]'], ["Total tests", "Total cases", "Total deaths"]))
 
     filters = []
     #filters.append(Filter(filter_type="DatePickerRange", column_name = "date"))
-    filters.append(Filter(filter_type="RangeSlider", column_name = "median_age"))
+    filters.append(Filter(filter_name = "Median age", filter_type="RangeSlider", column_name = "median_age"))
 
     filters.append(Filter(default_value = ["Norway"], column_name = "location", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "Cumulative tests, confirmed cases and deaths per million", axes = axes, filters = filters))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Cumulative tests, confirmed cases and deaths per million", axes = axes, filters = filters))
         
     graph_divs.append(new_custom_graph())
 
@@ -195,7 +233,7 @@ def add_preset(jsonified_data):
     filters = []
     filters.append(Filter(default_value = ["Italy"], column_name = "location", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "New cases by population", axes = axes, filters = filters))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "New cases by population", axes = axes, filters = filters))
         
     graph_divs.append(new_custom_graph())
 
@@ -208,7 +246,7 @@ def add_preset(jsonified_data):
     filters = []
     filters.append(Filter(default_value = ["Italy"], column_name = "location", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "New hospitalizations wrt the previous day", axes = axes, filters = filters))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "New hospitalizations wrt the previous day", axes = axes, filters = filters))
         
     graph_divs.append(new_custom_graph())
 
@@ -222,7 +260,7 @@ def add_preset(jsonified_data):
     filters = []
     filters.append(Filter(default_value = ["Italy"], column_name = "location", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "New ICUs wrt the previous day", axes = axes, filters = filters))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "New ICUs wrt the previous day", axes = axes, filters = filters))
         
     graph_divs.append(new_custom_graph())
 
@@ -236,7 +274,7 @@ def add_preset(jsonified_data):
     filters = []
     filters.append(Filter(default_value = ["Norway"], column_name = "location", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "Deaths per million", axes = axes, filters = filters))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Deaths per million", axes = axes, filters = filters))
         
     graph_divs.append(new_custom_graph())
     
@@ -249,7 +287,7 @@ def add_preset(jsonified_data):
     filters = []
     filters.append(Filter(default_value = ["Norway"], column_name = "location", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "New tests, confirmed cases and deaths per million", axes = axes, filters = filters))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "New tests, confirmed cases and deaths per million", axes = axes, filters = filters))
         
     graph_divs.append(new_custom_graph())
     
@@ -263,7 +301,7 @@ def add_preset(jsonified_data):
     filters.append(Filter(filter_type="DatePickerRange", column_name = "date"))
     filters.append(Filter(default_value = ["Norway"], column_name = "location", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "Cumulative tests, confirmed cases and deaths per million", axes = axes, filters = filters))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Cumulative tests, confirmed cases and deaths per million", axes = axes, filters = filters))
         
     graph_divs.append(new_custom_graph())
     
@@ -276,11 +314,11 @@ def add_preset(jsonified_data):
     filters = []
     #filters.append(Filter(default_value = ["2020-10-19"], column_name = "date", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "Total deaths per million", axes = axes, filters = filters, animation_frame = 'data["date"].astype(str)'))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Total deaths per million", axes = axes, filters = filters, animation_frame = 'data["date"].astype(str)'))
         
     graph_divs.append(new_custom_map())
 
-
+    
     # 5: Map with cases per million
     axes = []
     axes.append(Axis("x", True, 'data["location"]'))
@@ -288,10 +326,10 @@ def add_preset(jsonified_data):
 
     filters = []
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "Total cases per million", axes = axes, filters = filters))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Total cases per million", axes = axes, filters = filters))
         
     graph_divs.append(new_custom_map())
-
+    '''
 
 
     # 5: Deaths to cases ratio
@@ -300,7 +338,7 @@ def add_preset(jsonified_data):
     axes.append(Axis("y", True, ['data["total_deaths"]/data["total_cases"]'], ["Deaths to cases ratio"]))
 
     filters = []
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "Deaths to cases ratio", axes = axes, filters = filters, animation_frame = 'data["date"].astype(str)'))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Deaths to cases ratio", axes = axes, filters = filters, animation_frame = 'data["date"].astype(str)'))
         
     graph_divs.append(new_custom_map())
     
@@ -311,19 +349,19 @@ def add_preset(jsonified_data):
     axes.append(Axis("y", True, ['data["icu_patients"]/data["hosp_patients"]'], ["Deaths to cases ratio"]))
 
     filters = []
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "ICU to hospitalization ratio", axes = axes, filters = filters, animation_frame = 'data["date"].astype(str)'))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "ICU to hospitalization ratio", axes = axes, filters = filters, animation_frame = 'data["date"].astype(str)'))
         
     graph_divs.append(new_custom_map())
 
 
-    
+    '''
     # ICU to hospitalizations ratio
     axes = []
     axes.append(Axis("x", True, 'data["location"]'))
     axes.append(Axis("y", True, ['data["icu_patients"]/data["hosp_patients"]'], ["Deaths to cases ratio"]))
 
     filters = []
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "ICU to hospitalization ratio", axes = axes, filters = filters, animation_frame = 'data["date"].astype(str)'))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "ICU to hospitalization ratio", axes = axes, filters = filters, animation_frame = 'data["date"].astype(str)'))
         
     graph_divs.append(new_custom_map())
     '''
@@ -337,11 +375,55 @@ def add_preset(jsonified_data):
     filters = []
     filters.append(Filter(show_on_marker = True, default_value = ["Norway", "Italy", "Sweden"], column_name = "location", multi = True))
 
-    graph_infos.append(GraphInfo(dataset = jsonified_data, title = "Population density vs cases per million", axes = axes, filters = filters, hide_side_legend = True, plot_type = "markers"))
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Population density vs cases per million", axes = axes, filters = filters, hide_side_legend = True, plot_type = "markers"))
         
     graph_divs.append(new_custom_graph())
 
+
+
+    # Stringency and cases per million
+    axes = []
+    axes.append(Axis("Date", True, 'data["date"]'))
+    axes.append(Axis("Deaths and stringency", True, ['data["new_deaths_per_million"]', 'data["stringency_index"]'], ["Total deaths", "Stringency index"]))
+
+    filters = []
+
+    filters.append(Filter(default_value = ["Norway"], column_name = "location", multi = True))
+
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Deaths and stringency index through time", axes = axes, filters = filters))
+        
+    graph_divs.append(new_custom_graph())
+
+   
+
     
+    # Stringency / deaths (1 week shift)
+    axes = []
+    axes.append(Axis("Date", True, 'data["date"]'))
+    axes.append(Axis("Stringency per new deaths per million", True, ['data["stringency_index"]/(1 + data["new_deaths_per_million"].shift(periods = 7))'], ["Stringency index per new deaths"]))
+
+    filters = []
+    filters.append(Filter(default_value = ["Italy"], column_name = "location", multi = True))
+
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Stringency per new deaths (1 week shift)", axes = axes, filters = filters))
+        
+    graph_divs.append(new_custom_graph())
+
+
+    # Stringency / deaths (no shift)
+    axes = []
+    axes.append(Axis("Date", True, 'data["date"]'))
+    axes.append(Axis("Stringency per new deaths per million", True, ['data["stringency_index"]/(1 + data["new_deaths_per_million"])'], ["Stringency index per new deaths"]))
+
+    filters = []
+    filters.append(Filter(default_value = ["Italy"], column_name = "location", multi = True))
+
+    graph_infos.append(GraphInfo(dataset = jsonified_data,  title = "Stringency per new deaths (no shift)", axes = axes, filters = filters))
+        
+    graph_divs.append(new_custom_graph())
+
+
+
 
 
     # Initialize preset container and return
@@ -353,6 +435,7 @@ def new_custom_map():
     ind = len(graph_infos)-1
     graph_info = graph_infos[ind]
     covid_data = pd.read_json(graph_info.dataset, orient="split")
+
     covid_data['date'] = pd.to_datetime(covid_data['date'], dayfirst=True)
     covid_data.sort_values('date', ascending = True, inplace = True)
 
@@ -418,9 +501,17 @@ def new_custom_graph():
         elif(f.filter_type == "DatePickerRange"):
             filter_menu = dcc.DatePickerRange(id={'type': 'DP', 'index': ind, 'internal_index': subindex})
         elif(f.filter_type == "RangeSlider"):
-            filter_menu = dcc.RangeSlider(id =  {'type': 'SR', 'index': ind, 'internal_index': subindex}, min=data[f.column_name].min()*0.9, max=data[f.column_name].max()*1.1, step=(data[f.column_name].diff().max() - data[f.column_name].diff().min())/1000.0, value=[data[f.column_name].min(), data[f.column_name].max()])
+            filter_menu = dcc.RangeSlider(tooltip = {'always_visible': True, 'placement': 'bottom'}, dots = True, id =  {'type': 'SR', 'index': ind, 'internal_index': subindex}, min=data[f.column_name].min()*0.9, max=data[f.column_name].max()*1.1, step=(data[f.column_name].diff().max() - data[f.column_name].diff().min())/1000.0, value=[data[f.column_name].min(), data[f.column_name].max()])
         
-        graph_div.children.append(filter_menu)
+        filter_name = f.filter_name
+
+        filter_div = html.Div(className = "filterDiv", children = [])
+
+        if(filter_name != ""):
+            filter_div.children.append(html.Div(children = [filter_name]))
+        
+        filter_div.children.append(filter_menu)
+        graph_div.children.append(filter_div)
 
         if(f.filter_type == "Dropdown"):
         
@@ -447,7 +538,7 @@ def new_custom_graph():
 
         covid_data = data
         
-    fig.update_layout(showlegend=not graph_info.hide_side_legend, yaxis=dict(title=graph_info.axes[1].label), xaxis=dict(title=graph_info.axes[0].label))
+    fig.update_layout(height = graph_info.height, width = graph_info.width, showlegend=not graph_info.hide_side_legend, yaxis=dict(title=graph_info.axes[1].label), xaxis=dict(title=graph_info.axes[0].label))
 
     graph = dcc.Graph(id={'type': 'GR', 'index': ind}, figure=fig)
     graph.className = "graph_div graph"
